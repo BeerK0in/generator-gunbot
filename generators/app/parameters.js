228f613e3616ee838527ba7eed05ef5be8f4b4a8
BeerK0in <tbeerkoin@zant.one>
2017-05-13 02:47:23 +0200

    Increased the timeout between bot starts.

--- generators/app/parameters.js
+++ generators/app/parameters.js
@@ -1,6 +1,6 @@
 module.exports = {
   gunbotExeName: 'gunthy-linuxx64',
-  timeoutBetweenGunbotStarts: 3000,
+  timeoutBetweenGunbotStarts: 6000,
   markets: {
     poloniex: {name: 'poloniex'},
     kraken: {name: 'kraken'},
