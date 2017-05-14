6aac155ec587d2ed5e98aa1a97f0cc11ef531807
BeerK0in <tbeerkoin@zant.one>
2017-05-15 00:53:02 +0200

    Increased the timeout between bot starts even more. Reduced bot sleep time to 100s (down from 120s).

--- generators/app/parameters.js
+++ generators/app/parameters.js
@@ -1,6 +1,6 @@
 module.exports = {
   gunbotExeName: 'gunthy-linuxx64',
-  timeoutBetweenGunbotStarts: 6000,
+  timeoutBetweenGunbotStarts: 9000,
   markets: {
     poloniex: {name: 'poloniex'},
     kraken: {name: 'kraken'},
