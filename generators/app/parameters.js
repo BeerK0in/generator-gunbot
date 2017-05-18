65c86b3af6ee7686399608d2a0c25bfc01ee267e
BeerK0in <tbeerkoin@zant.one>
2017-05-18 16:08:39 +0200

    12 sec delay between starts.

--- generators/app/parameters.js
+++ generators/app/parameters.js
@@ -1,6 +1,6 @@
 module.exports = {
   gunbotExeName: 'gunthy-linuxx64',
-  timeoutBetweenGunbotStarts: 9000,
+  timeoutBetweenGunbotStarts: 12000,
   markets: {
     poloniex: {name: 'poloniex'},
     kraken: {name: 'kraken'},
